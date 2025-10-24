631d3fa09c3f234f7135aecc052d9b30f04bb967
Ranzsat <felixhoglund@hotmail.com>
2025-10-24 22:28:10 +0200

    made the main more dynamic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_num_words, 
     num_of_characters, 
@@ -9,7 +11,10 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = num_of_characters(text)
